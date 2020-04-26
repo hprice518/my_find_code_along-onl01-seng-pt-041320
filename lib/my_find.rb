f4a532fd4783b0678a2218fedc7f4e9388d0c016
Harrison Price <hprice518@gmail.com>
2020-04-26 18:26:48 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,8 +2,10 @@ require 'pry'
 
 def my_find(collection)
   i = 0
-  while i < collection.length return collection[i] if yield(collection[i])
-
+  while i < collection.length 
+    if yield(collection[i])
+    return collection[i]
+  end
     i = i + 1
   end
 end
